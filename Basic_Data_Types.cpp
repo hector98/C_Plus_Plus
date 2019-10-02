74f1182940c0505e3ac1d7121c5ed536d9051fbf
hector98 <barrioshector13@gmail.com>
2019-10-01 23:45:58 -0500

    Iniciamos, agragamos los problemas ya solucionados

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+  int i;
+  long l;
+  char ch;
+  float f;
+  double d;
+
+  scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+
+  printf("%d \n%ld \n%c \n%.3f \n%.9lf \n", i, l, ch, f, d);
+
+
+    return 0;
+}
